a574bac2c78444af1a70fe8c2387db3dd3efd1fa
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-15 09:20:51 +0530

    Speed JS: function

---
+++ 06_week/02_class/02_function.js
@@ -0,0 +1,38 @@
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+
+greet("Nirdesh");
+
+let globalVar = "I am Global";
+
+
+let globalVar = "I am global";
+
+function modifyGlobal() {
+  globalVar = "I am modified";
+  let blockScopedVar = "I am blocked-scoped";
+  console.log(blockScopedVar);
+}
+
+// modifyGlobal();
+
+let config = (function () {
+  let settings = {
+    theme: "dark",
+  };
+  return settings;
+})();
+
+let person1 = {
+  personsName: "ravi",
+  greet: function () {
+    console.log(`Hello, ${this.personsName}`);
+  },
+};
+
+let person2 = {
+  personsName: "hitesh",
+};
+
+person1.greet.call(person2);
