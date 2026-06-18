3dc7d8ad99db6163f79edc4cf9b8d7b893174f09
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-18 23:10:16 +0530

    Conversion

---
+++ 08_practice/03_conversion.js
@@ -0,0 +1,28 @@
+// conversion
+
+let score = false;
+let convertNumber = Number(score);
+
+console.log(convertNumber);
+console.log(typeof convertNumber);
+
+/*
+"22" = 22/Number
+"22abc" = NaN/Number
+"" = 0/Number
+true = 1/Number
+false = 0/Number
+*/
+
+let name = "n";
+let conertBoolean = Boolean(name);
+
+console.log(conertBoolean);
+console.log(typeof conertBoolean);
+
+/*
+0 = false
+1 = true
+"" = false
+"hitesh" = true
+*/
