a059b98993315d8aff7e44dc1565f4ea411e8b77
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-18 21:02:28 +0530

    variable

---
+++ 08_practice/01_variable.js
@@ -0,0 +1,12 @@
+// Variable
+
+const accountId = 1234;
+var name = "Nirdesh";
+let email = "nir@google.com";
+let city = "Lakhimpur Kheri";
+
+city = "Sitapur";  // value Reassignment kr sakte but variable Re-declaration nhi kr sakte
+// accountId = 0099;  // TypeError: Assignment to constant variable.
+// let email = "om"  // SyntaxError: Identifier 'email' has already been declared
+
+console.log(city);
